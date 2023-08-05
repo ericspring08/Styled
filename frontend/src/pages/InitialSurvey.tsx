e8aecb1d7177d6c53efdf7d67536142d62c17819
Eric Zhang <ericspring08@gmail.com>
2023-08-05 11:57:37 -0500

    finish design on page 1

--- frontend/src/pages/InitialSurvey.tsx
+++ frontend/src/pages/InitialSurvey.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import {useState} from "react";
@@ -6,16 +7,24 @@ export default function InitialSurvey ({surveyFinished}:any) {
   const [gender, setGender] = useState("mens");
   return (
     <>
-      <main className="flex flex-col justify-center items-center h-screen w-screen">
-        <h1 className="text-6xl">Step 1: Initial Survey</h1>
-        <h1 className="text-4xl">What is your Gender?</h1> 
-        <select className="select select-primary w-full max-w-xs" onChange={(e) => {
-          e.preventDefault();
-          setGender(e.target.value);
-        }}>
-          <option value="mens">Men</option>
-          <option value="womens">Women</option>
-        </select>
+      <main className="flex flex-col items-center justify-center h-screen w-screen">
+        <h1 className="text-6xl m-20">What is your Gender?</h1> 
+        <div className="flex flex-row items-center">
+          <select className="select select-primary select-lg w-full max-w-xs" onChange={(e) => {
+            e.preventDefault();
+            setGender(e.target.value);
+          }}>
+            <option value="mens">Men</option>
+            <option value="womens">Women</option>
+          </select>
+        </div>
+        <ul className="steps fixed z-90 top-10 item-center">
+          <li className="step step-primary">Inital Survey</li>
+          <li className="step">Body Measurements</li>
+          <li className="step">Clothing Selection</li>
+          <li className="step">Final Selection</li>
+        </ul>
+        
         <button onClick={() => {
           surveyFinished({
               gender: gender,
