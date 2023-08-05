fd2bbae57180b2fc4b7f05955c17e5f0ec61b66a
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-08-04 22:44:44 -0500

    .env for scraping

--- Scraping/firebase.js
+++ Scraping/firebase.js
@@ -1,30 +1,25 @@
 const womansFirestoreData = require("./womansFirestoreData.json")
 const { initializeApp } = require("firebase/app")
-const { getFirestore, setDoc, doc, deleteDoc } = require("firebase/firestore")
+const { getFirestore, setDoc, doc } = require("firebase/firestore")
+require("dotenv").config()
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDyOp2nmpxEtjsTx15IPP-rysLKZt_MJkQ",
-  authDomain: "styled-e.firebaseapp.com",
-  projectId: "styled-e",
-  storageBucket: "styled-e.appspot.com",
-  messagingSenderId: "669737929076",
-  appId: "1:669737929076:web:f182664beaaae427024421",
-  measurementId: "G-FK3S9Y4B19"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUERMENTID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
-
-/* for (let i = 0; i < 368; i++) {
-    deleteDoc(doc(db, "mens", womansFirestoreData[i].id)).then(() => {
-        console.log(i)
-    })
-} */
 
 womansFirestoreData.forEach((data, index) => {
     setDoc(doc(db, "womens", data.id), {
