9ec73cb9a03a986968baacf33b680ef81c12ca10
jeffsummer08 <jeff.k.zhou@gmail.com>
2023-08-05 12:36:13 -0500

    stuff

--- frontend/src/pages/BodyMeasure.tsx
+++ frontend/src/pages/BodyMeasure.tsx
@@ -4,14 +4,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import Webcam from "react-webcam";
-import {useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState } from "react";
 import { SquareLoader } from "react-spinners";
 import axios from "axios";
 
-export default function BodyMeasure({measureFinished}:any) {
-  const webCamRef = useRef<Webcam|null>(null);
+export default function BodyMeasure({ measureFinished }: any) {
+  const webCamRef = useRef<Webcam | null>(null);
   const [imgSrc, setImgSrc] = useState<string | null>(null);
-  const [size, setSize] = useState<string|null>(null);
+  const [size, setSize] = useState<string | null>(null);
   const [sizeIsLoading, setSizeIsLoading] = useState(false);
   const [loading, setLoading] = useState<boolean>(true)
   const [camera, setCamera] = useState<boolean>(false)
@@ -29,55 +29,59 @@ export default function BodyMeasure({measureFinished}:any) {
       const imageSrc = webCamRef.current?.getScreenshot();
       setSizeIsLoading(true);
       setSize(null);
-      if(imageSrc) {
+      if (imageSrc) {
         setImgSrc(imageSrc);
         axios.post("https://processshirtsize.onrender.com/processimage", {
           image: imageSrc
         })
-        .then((res) => {
-          if(Number(res.data) < 41) {
-            setSize("S");
-          } else if(Number(res.data) > 41 && Number(res.data) < 44) {
-            setSize("M");
-          } else if(Number(res.data) > 44 && Number(res.data) < 48) {
-            setSize("L");
-          } else if(Number(res.data) > 48 && Number(res.data) < 52) {
-            setSize("XL");
-          } else if(Number(res.data) > 52 && Number(res.data) < 56) {
-            setSize("XXL");
-          } else {
-            setSize("XXXL");
-          }
-        })
-        .then(() => {
-          setSizeIsLoading(false);
-        }).catch((err) => console.log(err));
+          .then((res) => {
+            if (Number(res.data) < 41) {
+              setSize("S");
+            } else if (Number(res.data) > 41 && Number(res.data) < 44) {
+              setSize("M");
+            } else if (Number(res.data) > 44 && Number(res.data) < 48) {
+              setSize("L");
+            } else if (Number(res.data) > 48 && Number(res.data) < 52) {
+              setSize("XL");
+            } else if (Number(res.data) > 52 && Number(res.data) < 56) {
+              setSize("XXL");
+            } else {
+              setSize("XXXL");
+            }
+          })
+          .then(() => {
+            setSizeIsLoading(false);
+          }).catch((err) => console.log(err));
       }
-      clearInterval(interval); 
+      clearInterval(interval);
     }, 3000);
   }
 
   return (
     <>
-      <h1>Please Allow Camera Access!</h1>
-      <Webcam height={600} ref={webCamRef}></Webcam>
-      <button onClick={capture} className="btn btn-primary">Take Photo</button>
-      {imgSrc && (
-        <img src={imgSrc} alt="User" />
-      )}
-      <SquareLoader
-          color={`hsl(var(--p))`}
-          loading={sizeIsLoading}
-          size={50}
-        />
       {
-        size && (
-          <h1 className="text-4xl">Size: {size}</h1>
-        )
+        !camera ? <h1>Please Allow Camera Access!</h1> :
+          <div className="flex flex-col justify-center items-center">
+            <Webcam height={600} ref={webCamRef}></Webcam>
+            <button onClick={capture} className="btn btn-primary">Take Photo</button>
+            {imgSrc && (
+              <img src={imgSrc} alt="User" />
+            )}
+            <SquareLoader
+              color={`hsl(var(--p))`}
+              loading={sizeIsLoading}
+              size={50}
+            />
+            {
+              size && (
+                <h1 className="text-4xl">Size: {size}</h1>
+              )
+            }
+            <button onClick={() => {
+              measureFinished();
+            }} className="fixed z-90 bottom-10 right-8 btn btn-primary drop-shadow-lg text-3xl hover:drop-shadow-2xl hover:animate-bounce duration-300">Continue</button>
+          </div>
       }
-      <button onClick={() => {
-        measureFinished();
-        }} className="fixed z-90 bottom-10 right-8 btn btn-primary drop-shadow-lg text-3xl hover:drop-shadow-2xl hover:animate-bounce duration-300">Continue</button>
     </>
   )
 }
