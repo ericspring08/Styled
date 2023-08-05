41eae847dad21f0aec12ae2751a4026af3a192c4
Eric Zhang <ericspring08@gmail.com>
2023-08-05 14:55:42 -0500

    fix

--- frontend/src/pages/BodyMeasure.tsx
+++ frontend/src/pages/BodyMeasure.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable @typescript-eslint/no-floating-promises */
